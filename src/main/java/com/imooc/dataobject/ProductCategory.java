9e3dd18e420b2aa3e55ec798cd4c75182df46149
Wang <504474011@qq.com>
2018-10-17 21:29:53 +0800

    springBoot点餐系统

---
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -0,0 +1,46 @@
+package com.imooc.dataobject;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 类目
+ */
+@Entity
+public class ProductCategory {
+    /** 类目id. */
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    /** 类目名字. */
+    private String categoryName;
+
+    /** 类目编号. */
+    private String categoryType;
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(String categoryType) {
+        this.categoryType = categoryType;
+    }
+}
